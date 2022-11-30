a4baee9b760a1d9d951c104dce0ff102f4b5bf25
masoom vali <masoomvali177@gmail.com>
2022-11-30 20:42:25 +0530

    create constructor in Song.java

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -9,10 +9,24 @@ package com.niit.jdp.model;
 public class Song {
 
     //attributes
-    private int id;
-    private String name;
+    private int songId;
+    private String songName;
     private String artist;
-    private String genre;
+    private String songGenre;
     private String album;
-    private float duration;
+    private double songDuration;
+    private String songPath;
+
+    public Song() {
+    }
+
+    public Song(int songId, String songName, String artist, String songGenre, String album, double songDuration, String songPath) {
+        this.songId = songId;
+        this.songName = songName;
+        this.artist = artist;
+        this.songGenre = songGenre;
+        this.album = album;
+        this.songDuration = songDuration;
+        this.songPath = songPath;
+    }
 }
