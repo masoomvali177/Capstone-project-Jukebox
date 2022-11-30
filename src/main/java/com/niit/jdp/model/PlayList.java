2eb591126219335c5ef8cc804e0fed630f96fa62
masoom vali <masoomvali177@gmail.com>
2022-11-30 20:47:48 +0530

    created getters and setters in PlayList.java

--- src/main/java/com/niit/jdp/model/PlayList.java
+++ src/main/java/com/niit/jdp/model/PlayList.java
@@ -18,4 +18,20 @@ public class PlayList {
         this.id = id;
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
