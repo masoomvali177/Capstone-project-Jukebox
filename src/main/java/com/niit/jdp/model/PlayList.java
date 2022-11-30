b75881c4e878c8287ee51b166e03cf3e737fefab
masoom vali <masoomvali177@gmail.com>
2022-11-30 20:48:17 +0530

    created toString() in PlayList.java

--- src/main/java/com/niit/jdp/model/PlayList.java
+++ src/main/java/com/niit/jdp/model/PlayList.java
@@ -34,4 +34,12 @@ public class PlayList {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "PlayList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
