76dddfb006b07592ce5538e70e9bac36c8066fc7
masoom vali <masoomvali177@gmail.com>
2022-12-01 16:45:58 +0530

    createdPlaylist() in PlayListRepository.java

--- src/main/java/com/niit/jdp/repository/PlayListRepository.java
+++ src/main/java/com/niit/jdp/repository/PlayListRepository.java
@@ -6,5 +6,51 @@
 
 package com.niit.jdp.repository;
 
+import com.niit.jdp.exception.InvalidNameException;
+import com.niit.jdp.model.PlayList;
+import com.niit.jdp.model.Song;
+import com.niit.jdp.service.DatabaseService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PlayListRepository {
+
+    Connection connection;
+    DatabaseService databaseService;
+    Scanner scanner=new Scanner(System.in);
+
+    public  PlayListRepository() throws SQLException, ClassNotFoundException {
+        databaseService = new DatabaseService();
+        connection = databaseService.getConnectionDatabase();
+    }
+    public void createPlaylist() {
+        String playlistName=scanner.nextLine();
+        String query="insert into `jukebox`.`playlist`(playlistName) values (?)";
+        try(PreparedStatement preparedStatement=connection.prepareStatement(query)){
+            preparedStatement.setString(1,playlistName);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
